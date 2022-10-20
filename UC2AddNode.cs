24cac16e0d5729e53e2b4787d8f00a10576fdb80
SurveshRathore <educatedevilboy@gmail.com>
2022-10-20 22:09:55 +0530

    create Linked List by adding 30 and 56 to 70

---
+++ UC2AddNode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14LinkedList
+{
+    internal class UC2AddNode
+    {
+        Node head = null;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                this.head = node;
+                
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
+    }
+}
